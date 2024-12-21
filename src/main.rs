1af6dcf5b58470039e8fe58669ed95d111e1b472
Dylan DPC <dylan.dpc@gmail.com>
2024-12-21 16:07:29 +0530

    works completely for add 1 to 1; bug fixed with binary calculation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 #![feature(let_chains)]
 #![feature(iter_map_windows)]
+#![deny(clippy::pedantic)]
 #![deny(rust_2018_idioms)]
+#![allow(clippy::missing_panics_doc)]
 
-use crate::sheet::{Sheet, Item};
+use crate::sheet::{Item, Sheet};
 
 pub mod sheet;
 
@@ -11,7 +13,7 @@ struct Pecker {
     sheets: Vec<Sheet>,
     input: Vec<Item>,
     cur: usize,
-    last: usize  
+    last: usize,
 }
 
 impl Pecker {
@@ -20,25 +22,24 @@ impl Pecker {
             sheets: vec![Sheet::default()],
             input,
             cur: 0,
-            last: 0, } }
+            last: 0,
+        }
+    }
     pub fn run(&mut self) {
         self.input.iter_mut().for_each(|item| {
             let cur = self.cur;
-        while self.sheets[cur].add_item(item).is_none() {
-            if cur == self.last {
-                self.last += 1;
+            while self.sheets[cur].add_item(item).is_none() {
+                if cur == self.last {
+                    self.last += 1;
+                }
+                self.cur += 1;
             }
-            self.cur += 1;
-        }
         });
     }
 }
 
-
-
-
 fn main() {
-    let input = vec![Item::new(1000,200,1), Item::new(200, 400, 1)];
+    let input = vec![Item::new(230, 200, 1), Item::new(310, 220, 1)];
     let mut pecker = Pecker::new(input);
     pecker.run();
     dbg!(pecker.sheets);
--- src/sheet.rs
+++ src/sheet.rs
@@ -4,21 +4,24 @@ pub const SHEET_WIDTH: u32 = 2400;
 pub const SHEET_HEIGHT: u32 = 1250;
 pub const TOOL_WIDTH: u32 = 10;
 
-
+#[must_use]
 pub const fn bit_length() -> u32 {
     SHEET_HEIGHT.ilog2() + width_bit_length()
 }
 
+#[must_use]
 pub const fn column_end() -> u32 {
     SHEET_WIDTH - 1
 }
 
+#[must_use]
 pub const fn row_end() -> u32 {
     SHEET_HEIGHT - 1
 }
 
+#[must_use]
 pub const fn width_bit_length() -> u32 {
-    SHEET_WIDTH.ilog2()
+    SHEET_WIDTH.ilog2() + 1
 }
 #[derive(Clone, Debug, Default)]
 pub struct Sheet {
@@ -26,56 +29,50 @@ pub struct Sheet {
 }
 
 impl Sheet {
-    pub fn add_item(&mut self, item: &mut Item) -> Option<()>{
+    pub fn add_item(&mut self, item: &mut Item) -> Option<()> {
         if self.entries.is_empty() {
-            self.entries.insert(0, PlacedItem::place_on_empty_sheet(item));
+            self.entries
+                .insert(0, PlacedItem::place_on_empty_sheet(item));
             Some(())
-        } else {
-            if let (Some(placed_item), position) = self.find_region(item) {
+        } else if let (Some(placed_item), position) = self.find_region(item) {
             self.entries.insert(position, placed_item);
             Some(())
-            } else {
-                todo!()
-            }
+        } else {
+            todo!()
         }
-
     }
 
     pub fn find_region(&mut self, item: &mut Item) -> (Option<PlacedItem>, u32) {
         if self.entries.len() > 1 {
-        let region = self.entries.iter().map_windows(|[(code, item), (next_code, next_item)] | {
-            todo!()
-
-        }).fold(None, |picked, item| {
-            todo!()
-        });
+            let region = self
+                .entries
+                .iter()
+                .map_windows(|[(code, item), (next_code, next_item)]| todo!())
+                .fold(None, |picked, item| todo!());
             (region, 0)
         } else {
             (self.find_adjacent_to_first_item(item), 1)
         }
-
     }
 
     pub fn find_adjacent_to_first_item(&self, item: &mut Item) -> Option<PlacedItem> {
-        item.align_item();
         let first = self.entries.iter().next().unwrap();
         let start = first.1.next_possible_column();
-        let boundary = self.check_item_with_boundary(&start, item);
-        match boundary {
-            Some(Orientation::Normalised) => {
-                Some(PlacedItem::new(item.to_owned(), start))
-            },
-            Some(Orientation::Flipped) =>  {
-            todo!()
-            },
-            None => todo!(),
+        if self.check_item_with_boundary(&start, item).is_some() {
+            Some(PlacedItem::new(item.to_owned(), start))
+        } else {
+            let start = first.1.next_possible_row();
+            self.check_item_with_boundary(&start, item)
+                .map(|_| PlacedItem::new(item.to_owned(), start))
         }
-
     }
 
-
-    pub fn check_item_with_boundary(&self, position: &Position, item: &mut Item) -> Option<Orientation> {
-        if dbg!(self.check_boundary(position, item)) {
+    pub fn check_item_with_boundary(
+        &self,
+        position: &Position,
+        item: &mut Item,
+    ) -> Option<Orientation> {
+        if self.check_boundary(position, item) {
             Some(Orientation::Normalised)
         } else {
             item.flip();
@@ -87,50 +84,49 @@ impl Sheet {
         }
     }
 
-
+    #[must_use]
     pub fn check_boundary(&self, start: &Position, item: &Item) -> bool {
-       let binary = start.binary();
-       let end = column_end();
-       let row_end = row_end();
-       (end - binary) > item.width && ((row_end - binary) << width_bit_length()) > item.height 
+        let binary = start.binary();
+        let column_end = column_end();
+        let row_end = row_end();
+        let width_bit = SHEET_WIDTH.next_power_of_two() - 1;
+        let height_bit = (SHEET_HEIGHT.next_power_of_two() - 1) << width_bit_length();
+
+        column_end.saturating_sub(binary & width_bit) > item.width
+            && (row_end.saturating_sub((binary & height_bit) >> width_bit_length())) > item.height
     }
-        
-
 }
-
 
 #[derive(Clone, Debug, Default)]
 pub struct Item {
-    height: u32,
     width: u32,
+    height: u32,
     count: u8,
 }
 
 impl Item {
-    pub fn new(height: u32, width: u32, count: u8) -> Self {
+    #[must_use]
+    pub fn new(width: u32, height: u32, count: u8) -> Self {
         Self {
-            height,
             width,
-            count
+            height,
+            count,
         }
     }
 
     pub fn align_item(&mut self) {
         if self.width < self.height {
-            self.flip()
+            self.flip();
         }
     }
 
     pub fn flip(&mut self) {
-            let width = self.width;
-            let height = self.height;
-            self.width = height;
-            self.height = width;
+        let width = self.width;
+        let height = self.height;
+        self.width = height;
+        self.height = width;
     }
-
-    
 }
-
 
 #[derive(Debug, Clone, Default)]
 pub struct PlacedItem {
@@ -140,45 +136,48 @@ pub struct PlacedItem {
 
 impl PlacedItem {
     fn new(item: Item, position: Position) -> Self {
-        Self {
-            item,
-            position,
-        }
+        Self { item, position }
     }
-    fn place_on_empty_sheet(item: &Item) -> Self {
-        PlacedItem::new(item.to_owned(), Position::new(0,0))
+    fn place_on_empty_sheet(item: &mut Item) -> Self {
+        item.align_item();
+        PlacedItem::new(item.to_owned(), Position::new(0, 0))
     }
 
-    fn next_possible_column(&self) -> Position{
-        Position::new(self.position.x + 1 + TOOL_WIDTH, self.position.y)
+    fn next_possible_column(&self) -> Position {
+        Position::new(
+            self.position.x + 1 + self.item.width + TOOL_WIDTH,
+            self.position.y,
+        )
     }
 
-
+    fn next_possible_row(&self) -> Position {
+        Position::new(
+            self.position.x,
+            self.position.y + self.item.height + TOOL_WIDTH,
+        )
+    }
 }
-
-
 
 #[derive(Clone, Debug, Default)]
 pub struct Position {
     x: u32,
-    y: u32
+    y: u32,
 }
 
 impl Position {
+    #[must_use]
     pub fn new(x: u32, y: u32) -> Self {
-        Self {
-            x,
-            y
-        }
+        Self { x, y }
     }
 
-    pub fn binary(&self) -> u32 {
-        self.y * bit_length() + self.x 
+    #[must_use]
+    pub const fn binary(&self) -> u32 {
+        self.y * SHEET_WIDTH.next_power_of_two() + self.x
     }
 }
 
 #[derive(Clone, Copy, Debug)]
 pub enum Orientation {
-   Normalised,
-   Flipped
+    Normalised,
+    Flipped,
 }
